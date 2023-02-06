98658bf5dac3770dfc1a81a0857e664e5552b542
Aleksandr <pandom@yandex.ru>
2023-02-06 23:43:36 +0300

    001 двумерный массив вещественных чисел

---
+++ 001/Program.cs
@@ -0,0 +1,28 @@
+﻿//Показать двумерный массив размером m×n заполненный вещественными числами
+Console.Clear();
+
+FillArray();       
+void FillArray()
+{
+Console.WriteLine("Введите размер двумерного массива m x n: ");
+Console.Write("- количество строк m = ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("- количество столбцов n = ");
+int n = int.Parse(Console.ReadLine()); 
+
+double[,] matr = new double[m, n];
+Random random = new Random();
+            
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        matr[i, j] = Math.Round((random.NextDouble() * 200 - 100), 2);
+        //matr[i, j] = Math.Round(random.Next(100) + random.NextDouble(), 3);
+        Console.Write("{0, 10}", (matr[i, j]).ToString("0.00"));
+    }
+Console.WriteLine();
+}
+}
+    
+
