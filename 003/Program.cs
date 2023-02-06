8d62e9bdbc2d303dbd364d217f929e9051194ec5
Aleksandr <pandom@yandex.ru>
2023-02-07 00:45:50 +0300

    003 Квадрат элемента с четными индексами

---
+++ 003/Program.cs
@@ -0,0 +1,45 @@
+﻿// В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+Console.Clear();
+
+Console.WriteLine("Введите размер двумерного массива m x n: ");
+Console.Write("количество строк m = ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("количество столбцов n = ");
+int n = int.Parse(Console.ReadLine()); 
+int[,] matr = new int[m, n];
+
+FillArray();
+Console.WriteLine();
+PowEvenArray();   
+
+void FillArray()
+{            
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        matr[i,j] = new Random().Next(1,10); // [1; 10)
+        Console.Write("{0, 5}", (matr[i, j]).ToString("0"));
+        //Console.Write($" {matr[i, j]} ");
+    }
+Console.WriteLine();
+}
+}
+
+void PowEvenArray()
+{            
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        if(i % 2 == 0 && j % 2 == 0)
+        {
+        matr[i,j] = matr[i,j] * matr[i,j];
+        }
+        Console.Write("{0, 5}", (matr[i, j]).ToString("0"));
+        //Console.Write($" {matr[i, j]} ");
+    }
+Console.WriteLine();
+}
+}
+
